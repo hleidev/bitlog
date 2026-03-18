47eb8a1ad8d10f8f31d63ae41e04f7ee3a0f64a7
harry <harryleiyyds@gmail.com>
2026-03-18 16:33:51 +0800

    feat(user-service): 初始化模块骨架，使用标准 Spring Profile 多环境配置

---
+++ bitlog-backend/bytelogs-user-service/src/main/java/top/harrylei/community/user/UserServiceApplication.java
@@ -0,0 +1,20 @@
+package top.harrylei.community.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 用户微服务启动类
+ *
+ * @author harry
+ * @since 0.0.1
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
