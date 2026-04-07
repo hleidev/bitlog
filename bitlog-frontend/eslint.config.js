62e716b56bd11af159b7a9698489b1bd0089652e
harry <harryleiyyds@gmail.com>
2026-04-07 18:54:40 +0800

    chore: init project scaffold
    Vue 3 + TypeScript + Vite 工程初始化，配置 ESLint、Prettier、路径别名及开发代理。

---
+++ bitlog-frontend/eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+)
